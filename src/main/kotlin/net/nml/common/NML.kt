6dacf44c14505dc60928c97e6a86a8b68b78fe20
PizzaCrust <torchprojectserver@gmail.com>
2016-04-29 19:18:16 +0700

    versioning

---
+++ src/main/kotlin/net/nml/common/NML.kt
@@ -0,0 +1,67 @@
+package net.nml.common
+
+/**
+ * The main class of NML Common.
+ *
+ * @since 1.0-SNAPSHOT
+ * @author PizzaCrust
+ */
+class NML {
+    /**
+     * NML's object module of Versioning.
+     */
+    companion object Versioning {
+        /**
+         * Retrieves the common's version.
+         */
+        fun getCommonVersion() : String {
+            return "1.0-SNAPSHOT"
+        }
+
+        /**
+         * Represents branches of the versioning.
+         */
+        enum class Branch {
+            /**
+             * Stable, and shouldn't have any bugs.
+             */
+            STABLE,
+
+            /**
+             * In development, and should have bugs. Should only be used for development.
+             */
+            DEV,
+        }
+
+        /**
+         * Retrieves the common's version branch enum.
+         */
+        fun getVersionBranch() : Branch {
+            return Branch.DEV
+        }
+
+
+        /**
+         * Commons has no implementation, show just output versioning info.
+         */
+        @JvmStatic fun main(args: Array<String>) {
+            if (args.size > 0) {
+                println("Arguments are not needed, why did you give arguments?")
+                println("Versioning data is only available in commons.")
+                val stringBuilder : StringBuilder = StringBuilder()
+                for (arg in args) {
+                    stringBuilder.append("'$arg', ")
+                }
+                println("Ignored these arguments: ${stringBuilder.toString()}")
+            }
+            val version = NML.getCommonVersion();
+            val branchEnum = NML.getVersionBranch();
+            var branchString = "STABLE";
+            when (branchEnum) {
+                Branch.DEV -> branchString = "DEV"
+            }
+            println("Commons is at $version.")
+            println("Branch: $branchString")
+        }
+    }
+}
