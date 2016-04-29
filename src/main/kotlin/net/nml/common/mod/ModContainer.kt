40cf0b999b20b83fdd52b94112943bdb458f3bef
PizzaCrust <torchprojectserver@gmail.com>
2016-04-29 21:13:33 +0700

    the container

---
+++ src/main/kotlin/net/nml/common/mod/ModContainer.kt
@@ -0,0 +1,45 @@
+package net.nml.common.mod
+
+/**
+ * Represents a mod container.
+ *
+ * @since 1.0-SNAPSHOT
+ * @author PizzaCrust
+ */
+class ModContainer(name: String, version: String, author: String, website: String, mod: IMod) {
+    /**
+     * Core mod info values
+     */
+    val name: String
+    val version: String
+    val author: String
+    val website: String
+
+    /**
+     * The implementation of the mod
+     */
+    val mod: IMod
+
+    init {
+        this.name = name
+        this.version = version
+        this.author = author
+        this.website = website
+        this.mod = mod
+    }
+
+    /**
+     * Mods need to implement this interface to be registered to the mod loader.
+     */
+    interface IMod {
+        /**
+         * Called when the mod is enabled.
+         */
+        fun onEnable()
+
+        /**
+         * Called when the mod is disabled
+         */
+        fun onDisable()
+    }
+}
