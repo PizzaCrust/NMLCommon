5d9efdaf6c41f7f2a85f45dd336fdc5b4af049bb
PizzaCrust <torchprojectserver@gmail.com>
2016-04-29 21:20:23 +0700

    security layer

---
+++ src/main/kotlin/net/nml/common/mod/SLayer.kt
@@ -0,0 +1,17 @@
+package net.nml.common.mod
+
+/**
+ * Represents the security layers of NML.
+ * This enum is according to the official NML specification document.
+ * --> See the documentation to get knowledge about this.
+ *
+ * @since 1.0-SNAPSHOT
+ * @author PizzaCrust
+ */
+enum class SLayer {
+    SECURE,
+    WARN,
+    DOUBTFUL,
+    BEWARE,
+    BLEACH
+}
