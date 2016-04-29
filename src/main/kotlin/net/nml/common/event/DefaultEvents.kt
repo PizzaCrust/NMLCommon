b66cf27beb75817166251a40c44a7f09ea54b55b
PizzaCrust <torchprojectserver@gmail.com>
2016-04-29 21:05:17 +0700

    default events class

---
+++ src/main/kotlin/net/nml/common/event/DefaultEvents.kt
@@ -0,0 +1,15 @@
+package net.nml.common.event
+
+/**
+ * Contains all the classes of the default events.
+ *
+ * @since 1.0-SNAPSHOT
+ * @author PizzaCrust
+ */
+class DefaultEvents {
+
+    // TODO: Work on this event, after mod loading.
+    class NMLLoadingModEvent : EventBus.Event {
+
+    }
+}
